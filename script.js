2c1eabcb99ad341d8c51f6030f04b93853fa86be
Glitch (hello-webpage) <none>
2020-07-26 05:33:43 +0000

    🦃💿 Checkpoint ./index.html:15784831/15773 ./style.css:15784831/1562 ./script.js:15784831/276

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+// coded by @ChaituVR
+const projectName = 'tribute-page';
+localStorage.setItem('example_project', 'Tribute Page');
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
